c0cce0160c6def51667a26f62e4d14104d5f5654
Harry Lincoln <harrylincoln@Harrys-MacBook-Pro.local>
2024-09-10 11:00:47 +0100

    NextJS with keycloak

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-# keycloak-nextjs-inform-poc
+# Keycloak inform tool using nextjs
+
+## Local keycloak
+```
+docker run -p 8080:8080 -e KEYCLOAK_ADMIN=admin -e KEYCLOAK_ADMIN_PASSWORD=admin quay.io/keycloak/keycloak:25.0.4 start-dev
+```
+
+Follow the step `A. Bootstrapping keycloak server` on this guide:
+https://medium.com/inspiredbrilliance/implementing-authentication-in-next-js-v13-application-with-keycloak-part-1-f4817c53c7ef
+
+
+## Local app 
+
+First, create `.env.local` from `.env.example` and plumb in the keycloak credentials.
+
+Second, run the development server:
+
+```bash
+npm run dev
+# or
+yarn dev
+# or
+pnpm dev
+# or
+bun dev
+```
+
+Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
---
+++ app/page.tsx
@@ -0,0 +1,50 @@
+import Image from "next/image";
+import { getServerSession } from "next-auth/next"
+import { authOptions } from "./api/auth/[...nextauth]/route"
+
+export default async function Home() {
+  const session = await getServerSession(authOptions)
+  return (
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+        <Image
+          className="dark:invert"
+          src="https://nextjs.org/icons/next.svg"
+          alt="Next.js logo"
+          width={180}
+          height={38}
+          priority
+        />
+        {/* {session ? (<p>Signed in as {session?.user?.email}</p>) : (<a href="/api/auth/signin">Sign in</a>)} */}
+      {session && <pre>{JSON.stringify(session, null, 2)}</pre>}
+
+        <div className="flex gap-4 items-center flex-col sm:flex-row">
+          <a
+            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
+            href="/api/auth/signin"
+            rel="noopener noreferrer"
+          >
+            <Image
+              className="dark:invert"
+              src="https://nextjs.org/icons/vercel.svg"
+              alt="Vercel logomark"
+              width={20}
+              height={20}
+            />
+            Login
+          </a>
+          {session && (
+          <a
+            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
+            href="/api/auth/signout"
+            rel="noopener noreferrer"
+          >
+            Logout
+          </a>)}
+        </div>
+      </main>
+      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
+      </footer>
+    </div>
+  );
+}
